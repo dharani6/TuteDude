a5c401cbbfbd2ddbaca3f075ae58a104c859a943
Dharanitharan <32707382+dharani6@users.noreply.github.com>
2025-07-01 21:20:02 +0530

    Assignment-3
    .

---
+++ Math_module.py
@@ -0,0 +1,5 @@
+from math import *
+num = int(input("enter a Number:"))
+print("Square root of the Number:",sqrt(num))
+print("algorithmic log base of e:",log(num))
+print("Sin of the number",sin(num))
